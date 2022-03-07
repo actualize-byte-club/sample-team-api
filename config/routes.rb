cdaaed4aed51545194848582cd94a6d34fd76585
Erin Jackson <erinebjackson@gmail.com>
2022-03-07 11:13:15 -0800

    adds jackson updates to the main file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   get "/pages" => "pages#index"
-<<<<<<< HEAD
   get "/newpages" => "new_pages#index"
 
 
-=======
   get "/bananas" => "bananas#index"
->>>>>>> 36f10d80dfcb79d8934298c39b11f49d249d0c3c
 end
